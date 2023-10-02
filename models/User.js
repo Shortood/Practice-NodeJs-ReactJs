6573f7812da10d9e771444c96e5835bbc147d3e1
Kang Woobin <woobin929@dgu.ac.kr>
2023-10-03 04:20:19 +0900

    토큰

--- models/User.js
+++ models/User.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +36,49 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567 암호화된 비밀번호 머시기
+
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken 이용해 token 생성
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+
+  user
+    .save()
+    .then(() => {
+      return cb(null, user);
+    })
+    .catch((err) => {
+      return cb(err);
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 
