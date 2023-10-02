f5426150ec6a6a7ce5c69d1012fcc1105e67b439
Kang Woobin <woobin929@dgu.ac.kr>
2023-10-03 03:00:23 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://woobin:rkd10230929!@cluster0.zri6zof.mongodb.net/').then(()=> console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+const config = require('./config/key');
 
-app.get('/', (req, res) => {
-  res.send('Hello World! kkk')
-})
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! kkk");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다
+
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
