e4d0f113b156db45c3973991c4ea6961a5213688
Kang Woobin <woobin929@dgu.ac.kr>
2023-11-27 22:38:36 +0900

    로그인 1

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,12 +1,44 @@
-import React, { useEffect } from "react";
-import axios from 'axios';
+import React, { useState } from "react";
 
 function LoginPage() {
-  useEffect(() => {
-    axios.get('/api/hello')
-    .then(response => console.log(response.data))
-  }, []);
-  return <div>LoginPage</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    //event.preventDefault();
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
